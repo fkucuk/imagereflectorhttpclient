81b832f5b8df0131af275520b15018ef723faa0b
FATIH KUCUK <FATIH.KUCUK@lcwaikiki.com>
2020-09-23 13:08:42 +0300

    init

---
+++ WebApplication1/Controllers/ImagesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ImagesController : ControllerBase
+    {
+       
+        public async Task<HttpResponseMessage> GetImage()
+        {
+            Uri uri = new Uri("https://img-lcwaikiki.mncdn.com/mnresize/1024/-/pim/productimages/20202/4353414/l_20202-0w9011z8-hrz_a.jpg", UriKind.Absolute);
+            using HttpClient client = new HttpClient();
+            return await client.GetAsync(uri);
+        }
+    }
+}
---
+++ WebApplication3/Controllers/ValuesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace WebApplication3.Controllers
+{
+    public class ValuesController : ApiController
+    {
+
+        [Route("images")]
+        public async Task<HttpResponseMessage> GetImage()
+        {
+            Uri uri = new Uri("https://img-lcwaikiki.mncdn.com/mnresize/1024/-/pim/productimages/20202/4353414/l_20202-0w9011z8-hrz_a.jpg", UriKind.Absolute);
+            using (HttpClient client = new HttpClient())
+            {
+                return await client.GetAsync(uri);
+            }
+        }
+    }
+}
